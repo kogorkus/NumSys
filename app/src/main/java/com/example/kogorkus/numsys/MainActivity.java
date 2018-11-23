ab07a63b174c89ed6d768913777d7f6440ba6734
kogorkus <kogorkus@mail.ru>
2018-11-23 03:04:40 +0300

    4

--- app/src/main/java/com/example/kogorkus/numsys/MainActivity.java
+++ app/src/main/java/com/example/kogorkus/numsys/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.kogorkus.numsys;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -13,10 +14,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    EditText edit;
     Button[] buttonsIn = new Button[4];
     Button[] buttonsOut = new Button[4];
-    int SystemIn =-1, SystemOut=-1;
+    int SystemIn = 0, SystemOut = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +31,15 @@ public class MainActivity extends AppCompatActivity {
         buttonsOut[1] = findViewById(R.id.ButOutHex);
         buttonsOut[2] = findViewById(R.id.ButOutOct);
         buttonsOut[3] = findViewById(R.id.ButOutBin);
-        EditText edit = (EditText)findViewById(R.id.ETinput);
+
+        edit = (EditText)findViewById(R.id.ETinput);
+        edit.getBackground().setColorFilter(0xFFA3C2F3, PorterDuff.Mode.SRC_IN);
         edit.addTextChangedListener(inputTW);
+
+
+
+
+
     }
 
     //region ButtonMethods
@@ -75,9 +83,9 @@ public class MainActivity extends AppCompatActivity {
     {
         for (int i = 0; i < 4; i++)
         {
-             buttonsIn[i].setBackgroundColor(0xFFD4D5D6);
+             buttonsIn[i].setBackgroundColor(0xFFA3C2F3);
         }
-        buttonsIn[number].setBackgroundColor(0xFF00FF00);
+        buttonsIn[number].setBackgroundColor(0xFF6CA7F4);
         SystemIn = number;
         EditText edit = (EditText)findViewById(R.id.ETinput);
         edit.setText(edit.getText().toString());
@@ -87,9 +95,9 @@ public class MainActivity extends AppCompatActivity {
     {
         for (int i = 0; i < 4; i++)
         {
-            buttonsOut[i].setBackgroundColor(0xFFD4D5D6);
+            buttonsOut[i].setBackgroundColor(0xFFA3C2F3);
         }
-        buttonsOut[number].setBackgroundColor(0xFF00FF00);
+        buttonsOut[number].setBackgroundColor(0xFF6CA7F4);
         SystemOut = number;
         EditText edit = (EditText)findViewById(R.id.ETinput);
         edit.setText(edit.getText().toString());
@@ -99,48 +107,71 @@ public class MainActivity extends AppCompatActivity {
 
 
     TextWatcher inputTW = new TextWatcher() {
-
         public void afterTextChanged(Editable s) {
-            EditText edit = (EditText)findViewById(R.id.ETinput);
+            edit = (EditText)findViewById(R.id.ETinput);
             String InputStr = edit.getText().toString();
             String OutputStr = "";
+            String CharStr = "";
             InputStr = InputStr.toUpperCase();
             int Calculatable = 0;
 
-            int BaseOfInSys = -1;
-            if(SystemIn == 0) BaseOfInSys = 10;
-            if(SystemIn == 1) BaseOfInSys = 16;
-            if(SystemIn == 2) BaseOfInSys = 8;
-            if(SystemIn == 3) BaseOfInSys = 2;
+            int BaseOfInSys = 0;
+            if(SystemIn == 0)
+            {
+                BaseOfInSys = 10;
+                CharStr = "0123456789";
+            }
+            if(SystemIn == 1)
+            {
+                BaseOfInSys = 16;
+
+                CharStr = "0123456789ABCDEF";
+            }
+            if(SystemIn == 2)
+            {
+                BaseOfInSys = 8;
+                CharStr = "01234567";
+            }
+            if(SystemIn == 3)
+            {
+                BaseOfInSys = 2;
+                CharStr = "01";
+            }
 
 
 
             int[] CalculatableArr = new int[InputStr.length()];
             for(int i = 0; i < InputStr.length(); i++)
             {
-                if(InputStr.charAt(i) == 'A') CalculatableArr[i] = 10;
-                if(InputStr.charAt(i) == 'B') CalculatableArr[i] = 11;
-                if(InputStr.charAt(i) == 'C') CalculatableArr[i] = 12;
-                if(InputStr.charAt(i) == 'D') CalculatableArr[i] = 13;
-                if(InputStr.charAt(i) == 'E') CalculatableArr[i] = 14;
-                if(InputStr.charAt(i) == 'F') CalculatableArr[i] = 15;
-                else CalculatableArr[i] =  Character.getNumericValue(InputStr.charAt(i));
+                if (!CharStr.contains(Character.toString(InputStr.charAt(i))))
+                {
+                    OutputStr = "Введены некорректные символы";
+                    break;
+                }
+                else if(InputStr.charAt(i) == 'A') CalculatableArr[i] = 10;
+                else if(InputStr.charAt(i) == 'B') CalculatableArr[i] = 11;
+                else if(InputStr.charAt(i) == 'C') CalculatableArr[i] = 12;
+                else if(InputStr.charAt(i) == 'D') CalculatableArr[i] = 13;
+                else if(InputStr.charAt(i) == 'E') CalculatableArr[i] = 14;
+                else if(InputStr.charAt(i) == 'F') CalculatableArr[i] = 15;
+                else
+                {
+                    CalculatableArr[i] =  Character.getNumericValue(InputStr.charAt(i));
+                }
+
             }
             for(int i = 0; i < CalculatableArr.length; i++)
             {
                 Calculatable += CalculatableArr[i] * Math.pow(BaseOfInSys, CalculatableArr.length - i -1);
             }
-            String str = "";
-            for (int i = 0; i < InputStr.length(); i++)
-            {
-                str += CalculatableArr[i] + " ";
-            }
 
-            if(SystemOut == 0) OutputStr = Integer.toString(Calculatable);
-            if(SystemOut == 1) OutputStr = Integer.toHexString(Calculatable);
-            if(SystemOut == 2) OutputStr = Integer.toOctalString(Calculatable);
-            if(SystemOut == 3) OutputStr = Integer.toBinaryString(Calculatable);
-            OutputStr = OutputStr.toUpperCase();
+            if(OutputStr != "Введены некорректные символы") {
+                if (SystemOut == 0) OutputStr = Integer.toString(Calculatable);
+                if (SystemOut == 1) OutputStr = Integer.toHexString(Calculatable);
+                if (SystemOut == 2) OutputStr = Integer.toOctalString(Calculatable);
+                if (SystemOut == 3) OutputStr = Integer.toBinaryString(Calculatable);
+                OutputStr = OutputStr.toUpperCase();
+            }
             TextView tview = (TextView)findViewById(R.id.TwResult);
             tview.setText(OutputStr);
         }
--- app/src/main/java/com/example/kogorkus/numsys/ThirdActivity.java
+++ app/src/main/java/com/example/kogorkus/numsys/ThirdActivity.java
@@ -1,7 +1,11 @@
 package com.example.kogorkus.numsys;
 
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -11,6 +15,7 @@ public class ThirdActivity extends AppCompatActivity {
     SeekBar seekBarInput;
     TextView textViewOutput;
     SeekBar seekBarOutput;
+    EditText editTextPro;
 
 
     @Override
@@ -21,39 +26,116 @@ public class ThirdActivity extends AppCompatActivity {
         textViewInput = (TextView) findViewById(R.id.txtViewInput);
         seekBarOutput = (SeekBar) findViewById(R.id.seekBarOutput);
         textViewOutput = (TextView) findViewById(R.id.txtViewOutput);
-        seekBarInput.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        editTextPro = (EditText)findViewById(R.id.editTextPro);
+        editTextPro.getBackground().setColorFilter(0xFFA3C2F3, PorterDuff.Mode.SRC_IN);
+        editTextPro.addTextChangedListener(inputTWPro);
 
-                textViewInput.setText(String.valueOf(progress + 2));
+
+        seekBarInput.setOnSeekBarChangeListener(SBListenerIn);
+        seekBarOutput.setOnSeekBarChangeListener(SBListenerOut);
+    }
+    SeekBar.OnSeekBarChangeListener SBListenerIn = new SeekBar.OnSeekBarChangeListener()
+    {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            textViewInput.setText(String.valueOf(progress + 2));
+            Convert();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+    SeekBar.OnSeekBarChangeListener SBListenerOut = new SeekBar.OnSeekBarChangeListener()
+    {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            textViewOutput.setText(String.valueOf(progress + 2));
+            Convert();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+    TextWatcher inputTWPro = new TextWatcher() {
+        public void afterTextChanged(Editable s) {
+            Convert();
+        }
+
+        public void beforeTextChanged(CharSequence s, int start, int count, int after){
+
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+    };
+    public void Convert()
+    {
+        editTextPro = (EditText)findViewById(R.id.editTextPro);
+        String InputStr = editTextPro.getText().toString();
+        String OutputStr = "";
+        String CharStr = "0123456789ABCDEF";
+        InputStr = InputStr.toUpperCase();
+        int Calculatable = 0;
+
+        int[] CalculatableArr = new int[InputStr.length()];
+        for(int i = 0; i < InputStr.length(); i++)
+        {
+            if (!CharStr.substring(0, seekBarInput.getProgress() + 2).contains(Character.toString(InputStr.charAt(i))))
+            {
+                OutputStr = "Введены некорректные символы";
+                break;
+            }
+            else if(InputStr.charAt(i) == 'A') CalculatableArr[i] = 10;
+            else if(InputStr.charAt(i) == 'B') CalculatableArr[i] = 11;
+            else if(InputStr.charAt(i) == 'C') CalculatableArr[i] = 12;
+            else if(InputStr.charAt(i) == 'D') CalculatableArr[i] = 13;
+            else if(InputStr.charAt(i) == 'E') CalculatableArr[i] = 14;
+            else if(InputStr.charAt(i) == 'F') CalculatableArr[i] = 15;
+            else
+            {
+                CalculatableArr[i] =  Character.getNumericValue(InputStr.charAt(i));
             }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+        for(int i = 0; i < CalculatableArr.length; i++)
+        {
+            Calculatable += CalculatableArr[i] * Math.pow(seekBarInput.getProgress() + 2, CalculatableArr.length - i -1);
+        }
 
+        if(OutputStr != "Введены некорректные символы") {
+            while (Calculatable > 0)
+            {
+                int temp = (Calculatable % (seekBarOutput.getProgress() + 2));
+                if(temp == 15) OutputStr = "F" + OutputStr;
+                else if(temp == 14) OutputStr = "E" + OutputStr;
+                else if(temp == 13) OutputStr = "D" + OutputStr;
+                else if(temp == 12) OutputStr = "C" + OutputStr;
+                else if(temp == 11) OutputStr = "B" + OutputStr;
+                else if(temp == 10) OutputStr = "A" + OutputStr;
+                else OutputStr = temp + OutputStr;
+                Calculatable /= seekBarOutput.getProgress() + 2;
             }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-        seekBarOutput.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-                textViewOutput.setText(String.valueOf(progress + 2));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
+            OutputStr = OutputStr.toUpperCase();
+        }
+        TextView tview = (TextView)findViewById(R.id.TVOutput);
+        tview.setText(OutputStr);
     }
 }
